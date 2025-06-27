a7aedc20bfd0db6862ecbb2bd62c1abe1210edfa
Jared Tan <wmis166@live.rhul.ac.uk>
2025-06-27 14:48:35 +0800

    Revert "Merge branch '5-Landing-Page' of https://github.com/Jayy-Ay/GroupieAi into 5-Landing-Page"
    This reverts commit 56238cf81a3ff0b67387d7215158502723eb6c98, reversing
    changes made to 7213c7a6d39f7a01b07be7171d8614f584d29c67.

--- SECURITY.md
+++
@@ -1,7 +0,0 @@
-# Security Policy
-
-## Supported Versions
-Latest version is always supported
-
-## Reporting a Vulnerability
-Report any vulnerabilities to Jayy-Ay on Github
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,11 +9,11 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-start justify-center">
-      <div className="flex flex-col items-start gap-16">
-        <header className="flex flex-col items-start gap-9">
-          <h1 className="text-left leading-relaxed text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Login to GroupieAi
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <header className="flex flex-col items-center gap-9">
+          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+            Welcome to <span className="sr-only">Remix</span>
           </h1>
           <div className="h-[144px] w-[434px]">
             <img
