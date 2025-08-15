d192e4244f46915eef9fc5bc77275e0eb0935d68
Jared Tan <wmis166@live.rhul.ac.uk>
2025-08-15 21:20:54 +0800

    #7 Updated ESlint and refactor folders

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// ESLint configuration for your project (ESLint v9+)
+
+/** @type {import('eslint').Linter.FlatConfig} */
+export default [
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'build/',
+      '*.tmp',
+      'generated/'
+    ],
+    rules: {
+      // Add your rules here
+    },
+  },
+];
